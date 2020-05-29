8b6e3ca77fb78e39f77560d13f0c1e598d4be9e8
yuu <yuuxzy@gmail.com>
2020-05-29 23:20:19 +0900

    minimum

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,7 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const WorkerPlugin = require("worker-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -10,9 +14,19 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
     ],
   },
   resolve: {
     extensions: [".js", ".ts", ".tsx", ".json", ".mjs", ".wasm"],
   },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname + "/../", "src/index.html"),
+    }),
+    new WorkerPlugin()
+  ],
 };
