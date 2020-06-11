881f19ae62a9526e0c80d8b1e60a319edc2d68b9
yuu <y.binsaki@kageyama-co.jp>
2020-06-11 22:30:39 +0900

    Fix typecheck

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
   Link,
 } from 'react-router-dom';
 
-const App: React.FunctionComponent<>  = () => {
+const App: React.FunctionComponent  = () => {
   return (
     <React.Fragment>
       <Router>
